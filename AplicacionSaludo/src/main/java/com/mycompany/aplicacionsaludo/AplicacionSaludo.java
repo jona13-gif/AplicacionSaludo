d87681c3f78b66516de07bb48df0cf17ced6e94d
jona <cornejojonathan056@gmail.com>
2026-08-23 10:15:21 -0500

    Creación inicial del proyecto y ventana principal
    Se creó el proyecto Java con Maven y la estructura inicial de la interfaz gráfica.

---
+++ AplicacionSaludo/src/main/java/com/mycompany/aplicacionsaludo/AplicacionSaludo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplicacionsaludo;
+
+/**
+ *
+ * @author corne
+ */
+public class AplicacionSaludo {
+
+    
+    }
+
